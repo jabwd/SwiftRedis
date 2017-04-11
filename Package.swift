3aff9cd9b2c433b839e3a0bbbd4ed83ff276b1b1
Antwan van Houdt <jabwd@exurion.com>
2017-04-11 16:56:53 +0200

    Minor change

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-	name:  "SwiftRedis",
+	name: "SwiftRedis",
 	dependencies: [
 		.Package(url:  "https://github.com/jabwd/RedisC", versions: Version(0,1,0)..<Version(1,0,0))
 	]
