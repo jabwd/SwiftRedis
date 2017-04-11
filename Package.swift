a49424c8e30d2c42c9cd3dde5a9f640fabdd1196
Antwan van Houdt <jabwd@exurion.com>
2017-04-11 17:00:34 +0200

    Removed RedisC dependency, shouldnt be needed anymore!

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@ import PackageDescription
 
 let package = Package(
 	name: "SwiftRedis",
-	dependencies: [
+	/*dependencies: [
 		.Package(url:  "https://github.com/jabwd/RedisC", versions: Version(0,1,0)..<Version(1,0,0))
-	]
+	]*/
 )
